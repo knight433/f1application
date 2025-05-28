2a15d50b37749697fc8c675c076fed134649fda5
Dhruva U <dhruva.u@mphasis.com>
2025-05-28 13:19:08 +0530

    add RacePage component and integrate routing from RaceCard

--- web/src/components/RaceCard.js
+++ web/src/components/RaceCard.js
@@ -1,9 +1,18 @@
 // src/components/RaceCard.js
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 const RaceCard = ({ raceName, time }) => {
+
+  const navigate = useNavigate();
+
+  const handleclick = () => {
+    navigate(`/pages/racePage/${encodeURIComponent(raceName)}`);
+  }
+
   return (
-    <div className="card bg-dark text-white m-2" style={{ width: '18rem', borderRadius: '12px', border: '1px solid #6f42c1' }}>
+    <div className="card bg-dark text-white m-2" style={{ width: '18rem', borderRadius: '12px', border: '1px solid #6f42c1' }} onClick={handleclick}>
       <div className="card-body">
         <h5 className="card-title">{raceName}</h5>
         <p className="card-text">{time}</p>
